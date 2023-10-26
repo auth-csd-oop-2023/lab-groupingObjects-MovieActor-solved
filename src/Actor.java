a59fa94720fe5ba3e12bbcb0454b65f27cc1a96b
gmeditsk <gmeditsk@csd.auth.gr>
2023-10-27 00:55:56 +0300

    Initial commit

---
+++ src/Actor.java
@@ -0,0 +1,84 @@
+import java.util.ArrayList;
+
+/**
+ * Αυτή η κλάση αναπαριστά έναν/μία ηθοποιό με το όνομα του/της, την ηλικία του/της και την λίστα με τις ταινίες
+ * στις οποίες έχει παίξει.
+ * This class represents an actor/actress with his/her name, age and list of movies he/she participated.
+ */
+public class Actor {
+    private String name;
+    private int age;
+    private ArrayList<Movie> movies;
+
+    /**
+     * Κατασκευαστής - Constructor
+     */
+    public Actor(String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.movies = new ArrayList<>();
+
+    }
+
+    /**
+     * Αυτή η μέθοδος επιστρέφει το όνομα του ηθοποιού.
+     * This method returns the actor's name.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Αυτή η μέθοδος επιστρέφει την ηλικία του ηθοποιού.
+     * This method returns the actor's age.
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * Αυτή η μέθοδος προσθέτει μια ταινία στην λίστα ταινιών του ηθοποιού.
+     * This methods adds a movie to hir/her lists of movies.
+     */
+    public void addMovie(String title, int duration, int oscars, int budget) {
+        this.movies.add(new Movie(title, duration, oscars, budget));
+    }
+
+    /**
+     * Αυτή η μέθοδος επιστρέφει τις ταινίες στις οποίες έχει παίξει ο ηθοποιός.
+     * This methods returns the movies the actor has participated.
+     */
+    public ArrayList<Movie> getMovies() {
+        return this.movies;
+    }
+
+    /**
+     * Αυτή η μέθοδος υπολογίζει τον χρόνο σε λεπτά που ο/η ηθοποιός έχει παίξει σε ταινίες μεγάλου μήκους (>1 ώρα)
+     * This method computes the total play time of the actor in long movies (>1 hour)
+     */
+    public int totalActingTime() {
+        int sum = 0;
+        for (Movie movie : this.movies) {
+            if (movie.getDuration() > 60) {
+                sum += movie.getDuration();
+            }
+        }
+        return sum;
+    }
+
+    /**
+     * Αυτή η μέθοδος υπολογίζει την "δημοφιλία" του/της ηθοποιού. Η δημοφιλία υπολογίζεται ως ο συνολικός αριθμός των
+     * ταινιών που έχει παίξει προς τον αριθμό των οσκαρικών ταινιών που έχει παίξει.
+     * This method computes the "popularity" of the actor. The popularity is the total number of movies he/she
+     * participated to the total number of oscar-winning movies he/she participated.
+     */
+    public double popularity() {
+        int totalOscars = 0;
+        for (Movie movie : this.movies) {
+            if (movie.getOscars() > 0) {
+                totalOscars += 1;
+            }
+        }
+        return this.movies.size() / (double) totalOscars;
+    }
+}
---
+++ src/Movie.java
@@ -0,0 +1,66 @@
+/**
+ * Αυτή η κλάση αναπαριστά μια ταινία. Κάθε ταινία περιέχει έναν τίτλο, διάρκεια σε λεπτά, αριθμό oscars που έχει
+ * κερδίσει και το συνολικό ποσό που ξοδεύτηκε από την παραγωγή σε εκκατομύρια.
+ * This class represents a movie. Each movie contains a title, its duration in minutes, the number of oscars won and
+ * the total amount spent from the production in millions.
+ */
+public class Movie {
+    private String title;
+    private int duration;
+    private int oscars;
+    private int budget;
+
+    /**
+     * Κατασκευαστής - Constructor
+     */
+    public Movie(String title, int duration, int oscars, int budget) {
+        this.title = title;
+        this.duration = duration;
+        this.oscars = oscars;
+        this.budget = budget;
+    }
+
+    /**
+     * Αυτή η μέθοδος επιστρέφει το τίτλο της ταινίας.
+     * This method returns the movie's title.
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * Αυτή η μέθοδος επιστρέφει την διάρκεια της ταινίας.
+     * This movie returns the movie's duration.
+     */
+    public int getDuration() {
+        return duration;
+    }
+
+    /**
+     * Αυτή η μέθοδος επιστρέφει τα oscars που έχει κερδίσει η ταινία.
+     * This method returns the total oscars the movie won.
+     */
+    public int getOscars() {
+        return oscars;
+    }
+
+    /**
+     * Αυτή η μέθοδος επιστρέφει το συνολικό ποσό που ξοδεύτηκε από την παραγωγή.
+     * This method returns the total amount spent by the production.
+     */
+    public int getBudget() {
+        return budget;
+    }
+
+    /**
+     * Αυτή η μέθοδος υπολογίζει τον δείκτη oscars - το συνολικό budget προς τον αριθμό των oscars.
+     * This method returns the oscar index - the total budget to the number of oscars won.
+     * double value = (double) value
+     */
+    public double oscarIndex() {
+        if (this.oscars > 0) {
+            return this.budget / (double) this.oscars;
+        }
+        return 0;
+    }
+}
